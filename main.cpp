ddc94502ba34d95504c52ba5ad986ccdd010ea30
MaXiMuS-35 <maksimus.sergeyev@gmail.com>
2023-11-18 17:56:01 +0300

    minor improvements

--- main.cpp
+++ main.cpp
@@ -16,6 +16,8 @@ CPU:
 	L1 cache:	640 KB
 	L2 cache:	4,0 MB
 	L3 cache:	16,0 MB
+
+	//micro architecture - rocket lake
 ICC
 options: /permissive /GS /Qopenmp /Qftz /W3 /QxHost /Gy /Zc:wchar_t /Zi /O3 /Fd"x64\Release\vc143.pdb" /Zc:inline /Quse-intel-optimized-headers /D "NDEBUG" /D "_CONSOLE" /D "_UNICODE" /D "UNICODE" /Qipo /Zc:forScope /Oi /MD /FC /Fa"x64\Release\" /EHsc /nologo /Qparallel /Fo"x64\Release\" /Ot /Fp"x64\Release\matrix_multiplication.pch" 
 
